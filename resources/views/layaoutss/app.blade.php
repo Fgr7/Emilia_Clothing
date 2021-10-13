c57c9cb8bc630d2441eeb2c20c740e1594bce31f
juan <juanjosefgr@hotmail.com>
2021-10-12 20:50:03 -0500

    primera versión del loginUser y pagina de inicio

---
+++ resources/views/layaoutss/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title') - Laravel App</title>
+    
+    <!-- Tailwind CSS Link -->
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+  </head>
+  <body class="bg-gray-100 text-gray-800">
+    <!-- Document body -->
+    <nav class="flex py-5 bg-pink-300 text-white"> 
+        <div class="w-1/2 px-12 mr-auto">
+            <p class="text-4xl front-bold text-pink-500 font-serif">Emilia</p>
+        </div>
+        <ul class="w-1/2 px-16 ml-auto flex justify-end pt-l">
+          @if(auth()->check())
+            <li class="mx-6">
+                <p class="text-xl text-pink-400 font ">Bienvenido   <b>{{ auth()->user()->name }}</b></p>
+            </li>
+
+            <li>
+                <a href="{{ route('login.destroy')}}" class= "front-bold border-2 border-white py-3 px-4 rounded-md hover:bg-white hover:text-pink-700">Log out</a>
+            </li>
+          @else
+          <li class="mx-1">
+                <a href="{{ route('login.index')  }}" class="font-semibold hover:bg-pink-700 py-3 px-4 rounded-md">Log In</a>
+            </li>
+
+            <li>
+                <a href="{{ route('register.index')  }}" class= "front-semibold border-2 border-white py-2 px-4 rounded-md hover:bg-white hover:text-pink-700">Register</a>
+            </li>
+          @endif
+        </ul>  
+
+    </nav>
+   
+      
+    @yield('content')
+
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login',[SessionsController::class,'create'])
+    ->middleware('guest')
+    ->name('login.index');
+
+Route::post('/login',[SessionsController::class,'store'])
+    ->name('login.store');
+
+Route::get('/logout',[SessionsController::class,'destroy'])
+    ->middleware('auth')
+    ->name('login.destroy');
+
+Route::post('/register',[RegisterController::class,'store'])
+    ->name('register.store');
+
+Route::get('/register',[RegisterController::class,'create'])
+    ->middleware('guest')
+    ->name('register.index');
