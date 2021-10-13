fd02301d866a8f8e8973bdbb61ae34b5700c4380
judiespa07 <judiespa07@gmail.com>
2021-10-13 15:06:04 -0500

    again

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-<<<<<<< HEAD
 use App\Http\Controllers\ContactoController;
 use App\Http\Controllers\HomeController;
-=======
 use App\Http\Controllers\InfoController;
->>>>>>> a88fc442c2a6666f6baf54b51bd428128d200dda
 
 
 route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('contacto', [ContactoController::class, 'index'])->name('contacto.index');
+
 
 Route::get('/login',[SessionsController::class,'create'])
     ->middleware('guest')
@@ -31,13 +29,3 @@ Route::post('/register',[RegisterController::class,'store'])
 Route::get('/register',[RegisterController::class,'create'])
     ->middleware('guest')
     ->name('register.index');
-<<<<<<< HEAD
-=======
-
-Route::get('/descripcion', [InfoController::class, 'create'])
-    ->name('info.create');
-
-Route::get('/info',[InfoController::class,'create'])
-    ->middleware('guest')
-    ->name('info.index');   
->>>>>>> a88fc442c2a6666f6baf54b51bd428128d200dda
